91bd4d0c10608ede559e12194cb6712109289df0
ALFREDO VARON MUÑOZ <jmunozav@jci.com>
2026-07-25 05:24:20 -0500

    correccion de url const supabaseUrl = 'https://zvncdwhnphzmskyeizne.supabase.co';

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zvncdwhnphzmskyeizne.supabase.co/rest/v1/';
+const supabaseUrl = 'https://zvncdwhnphzmskyeizne.supabase.co';
 const supabaseAnonKey = 'sb_publishable_9p-Ta0F3YrzTbmKGRMc6dg_cuNVDQKF';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
